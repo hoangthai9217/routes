1a4101a913f29889d6c5b956f387898128c418d2
Hoang Thai <thai.nh@ascendcorp.com>
2020-07-10 12:01:41 +0700

    Add coordinator

---
+++ GapoRoutes/Coordinator.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinator.swift
+//  GapoRoutes
+//
+//  Created by peanut36k on 10/7/20.
+//  Copyright © 2020 peanut36k. All rights reserved.
+//
+
+import Foundation
+
+class Coordinator {
+    
+}
